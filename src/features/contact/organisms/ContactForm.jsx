5fd44a5bbbd934fbcafededdac88c4497668affa
Lautaro <lautarofaridtoledo@gmail.com>
2023-05-03 02:55:43 -0300

    feat: contact section with form

---
+++ src/features/contact/organisms/ContactForm.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {CButton, CCol, CForm, CFormCheck, CFormInput, CFormSelect, CFormTextarea} from "@coreui/react";
+
+const ContactForm = () =>
+{
+    return(
+        <CForm className="row g-3">
+            <CCol lg={6} md={6}>
+                <CFormInput type="text" id="inputName" label="Nombre" placeholder={'Tu nombre...'}/>
+            </CCol>
+            <CCol lg={6} md={6}>
+                <CFormInput type="email" id="inputEmail4" label="Email" placeholder={'Tu email...'}/>
+            </CCol>
+            <CCol lg={6} md={8}>
+                <CFormInput type="number" id="inputPhone" label="Telefono" placeholder={'Tu numero...'}/>
+            </CCol>
+            <CCol lg={12} md={12}>
+                <CFormTextarea label="Mensaje" id="inputMessage" placeholder="Escribe tu mensaje aquí" style={{resize: "none"}}/>
+            </CCol>
+
+            <CCol xs={12}>
+                <CButton type="submit">Enviar</CButton>
+            </CCol>
+        </CForm>
+    )
+}
+
+export default ContactForm;
